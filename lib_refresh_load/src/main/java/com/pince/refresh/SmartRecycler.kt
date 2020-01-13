34f67e4b2a775f571117cc28b5b66d570e5fa440
manjiale <1013666753@qq.com>
2020-01-13 15:48:24 +0800

    101

--- lib_refresh_load/src/main/java/com/pince/refresh/SmartRecycler.kt
+++ lib_refresh_load/src/main/java/com/pince/refresh/SmartRecycler.kt
@@ -13,7 +13,7 @@ import com.scwang.smartrefresh.layout.api.RefreshHeader
 /**
  * 刷新分页控制
  */
-class SmartRecycler:FrameLayout {
+class SmartRecycler<T>:FrameLayout {
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
@@ -23,7 +23,7 @@ class SmartRecycler:FrameLayout {
 
     private  var smartRefreshLayout: SmartRefreshLayout
     private  var flRecyContent:FrameLayout
-    private lateinit var smartRefreshHelper:SmartRefreshHelper<*>
+    private lateinit var smartRefreshHelper:SmartRefreshHelper<T>
 
 
     init {
@@ -43,16 +43,31 @@ class SmartRecycler:FrameLayout {
     }
 
 
+    fun onFetchDataError(){
+
+    }
+
+    fun onFetchDataFinish(data: List<T>?, goneIfNoData: Boolean=true){
+        smartRefreshHelper.onFetchDataFinish(data ,true)
+    }
 
     /**
      * 初始化
+     *
+     * @param emptyView 空视图
+     *
+     *@param preLoadNumber 静默加载滑到倒数第几个开始
+     * @param fetcherFuc 刷新页回调　0开始
      */
-    fun setUp(adapter: BaseQuickAdapter<*, *> , emptyView:IEmptyView?=null,preLoadNumber: Int=1,loadmoreNeed:Boolean=true,refreshNeed:Boolean=true,fetcherFuc: (page: Int) -> Unit){
+    fun setUp(adapter: BaseQuickAdapter<T, *> , emptyView:IEmptyView?=null,preLoadNumber: Int=1,loadmoreNeed:Boolean=true,refreshNeed:Boolean=true,fetcherFuc: (page: Int) -> Unit){
         emptyView?.let {
             flRecyContent.addView(it.contentView)
         }
         recyclerView.adapter = adapter
         smartRefreshHelper = SmartRefreshHelper(adapter,recyclerView,smartRefreshLayout,emptyView,preLoadNumber,loadmoreNeed,refreshNeed,fetcherFuc)
     }
+
+
+
 
 }
